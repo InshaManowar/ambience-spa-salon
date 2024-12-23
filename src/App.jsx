0d07adce519fd4ca7d407eab250f692324dc6b44
Insha Manowar <inshamanowar@Inshas-MacBook-Pro.local>
2024-12-23 14:45:52 +0530

    loader, images compressed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainSite from './components/MainSite';
 import AdminPanel from './components/AdminPanel';
@@ -7,38 +7,53 @@ import ContactPage from './components/ContactPage';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import BackToTop from './components/BackToTop';
+import LoadingScreen from './components/LoadingScreen';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate minimum loading time of 2 seconds
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={
-          <>
-            <Navbar />
-            <MainSite />
-            <Footer />
-            <BackToTop />
-          </>
-        } />
-        <Route path="/services" element={
-          <>
-            <Navbar />
-            <ServicesPage />
-            <Footer />
-            <BackToTop />
-          </>
-        } />
-        <Route path="/book" element={
-          <>
-            <Navbar />
-            <ContactPage />
-            <Footer />
-            <BackToTop />
-          </>
-        } />
-        <Route path="/admin" element={<AdminPanel />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <LoadingScreen isLoading={isLoading} />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Navbar />
+              <MainSite />
+              <Footer />
+              <BackToTop />
+            </>
+          } />
+          <Route path="/services" element={
+            <>
+              <Navbar />
+              <ServicesPage />
+              <Footer />
+              <BackToTop />
+            </>
+          } />
+          <Route path="/book" element={
+            <>
+              <Navbar />
+              <ContactPage />
+              <Footer />
+              <BackToTop />
+            </>
+          } />
+          <Route path="/admin" element={<AdminPanel />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
