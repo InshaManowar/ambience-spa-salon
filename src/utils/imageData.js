e3ddae911885412e18b33e6511a7f913965eeba3
Insha Manowar <inshamanowar@Inshas-MacBook-Pro.local>
2024-12-24 09:52:56 +0530

    loader, images compressed

--- src/utils/imageData.js
+++ src/utils/imageData.js
@@ -1,63 +1,64 @@
 // Hero slider image data
-export const images = [
+export const heroImages = [
     {
-      mobile: {
-        src: "https://i.ibb.co/Y7d3TZ2/7.png",
-        alt: "Luxury Hair Styling"
+        mobile: {
+          src: "https://i.ibb.co/Y7d3TZ2/7.png",
+          alt: "Luxury Hair Styling"
+        },
+        desktop: {
+          src: "https://i.ibb.co/RHbJFZY/1.png",
+          alt: "Luxury Hair Styling"
+        }
       },
-      desktop: {
-        src: "https://i.ibb.co/RHbJFZY/1.png",
-        alt: "Luxury Hair Styling"
-      }
-    },
-    {
-      mobile: {
-        src: "https://i.ibb.co/chd538z/6.png",
-        alt: "Salon Interior"
+      {
+        mobile: {
+          src: "https://i.ibb.co/chd538z/6.png",
+          alt: "Salon Interior"
+        },
+        desktop: {
+          src: "https://i.ibb.co/6NHm8v8/4-1.png",
+          alt: "Salon Interior"
+        }
       },
-      desktop: {
-        src: "https://i.ibb.co/6NHm8v8/4-1.png",
-        alt: "Salon Interior"
-      }
-    },
-    {
-      mobile: {
-        src: "https://images.pexels.com/photos/8467976/pexels-photo-8467976.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-        alt: "Salon Interior"
+      {
+        mobile: {
+          src: "https://images.pexels.com/photos/8467976/pexels-photo-8467976.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Salon Interior"
+        },
+        desktop: {
+          src: "https://images.pexels.com/photos/8467976/pexels-photo-8467976.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Salon Interior"
+        }
       },
-      desktop: {
-        src: "https://images.pexels.com/photos/8467976/pexels-photo-8467976.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-        alt: "Salon Interior"
-      }
-    },
-    {
-      mobile: {
-        src: "https://images.pexels.com/photos/1845160/pexels-photo-1845160.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-        alt: "Salon Interior"
+      {
+        mobile: {
+          src: "https://images.pexels.com/photos/1845160/pexels-photo-1845160.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Salon Interior"
+        },
+        desktop: {
+          src: "https://images.pexels.com/photos/1845160/pexels-photo-1845160.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+          alt: "Salon Interior"
+        }
       },
-      desktop: {
-        src: "https://images.pexels.com/photos/1845160/pexels-photo-1845160.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-        alt: "Salon Interior"
-      }
-    },
-    {
-      mobile: {
-        src: "https://i.ibb.co/30LXFp3/2.png",
-        alt: "men hair"
+      {
+        mobile: {
+          src: "https://i.ibb.co/30LXFp3/2.png",
+          alt: "men hair"
+        },
+        desktop: {
+          src: "https://i.ibb.co/30LXFp3/2.png",
+          alt: "men hair"
+        }
       },
-      desktop: {
-        src: "https://i.ibb.co/30LXFp3/2.png",
-        alt: "men hair"
+      {
+        mobile: {
+          src: "https://i.ibb.co/7X165XH/3-2.png",
+          alt: "men hair"
+        },
+        desktop: {
+          src: "https://i.ibb.co/7X165XH/3-2.png",
+          alt: "men hair"
+        }
       }
-    },
-    {
-      mobile: {
-        src: "https://i.ibb.co/7X165XH/3-2.png",
-        alt: "men hair"
-      },
-      desktop: {
-        src: "https://i.ibb.co/7X165XH/3-2.png",
-        alt: "men hair"
-      }
-    }
   ];
+
