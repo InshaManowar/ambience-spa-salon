ccdf33100156f1e4594131f13db50af0c5a7aab2
Insha Manowar <inshamanowar@Inshas-MacBook-Pro.local>
2024-12-24 09:51:37 +0530

    loader, images compressed

--- src/utils/imageData.js
+++ src/utils/imageData.js
@@ -1,42 +1,62 @@
 // Hero slider image data
-export const heroImages = [
+export const images = [
     {
       mobile: {
-        src: "https://www.simpleimageresizer.com/_uploads/photos/a5591a2e/7_1_optimized.png",
+        src: "https://i.ibb.co/Y7d3TZ2/7.png",
         alt: "Luxury Hair Styling"
       },
       desktop: {
-        src: "https://www.simpleimageresizer.com/_uploads/photos/a5591a2e/1_optimized.png",
+        src: "https://i.ibb.co/RHbJFZY/1.png",
         alt: "Luxury Hair Styling"
       }
     },
     {
       mobile: {
-        src: "https://www.simpleimageresizer.com/_uploads/photos/a5591a2e/6_optimized.png",
+        src: "https://i.ibb.co/chd538z/6.png",
         alt: "Salon Interior"
       },
       desktop: {
-        src: "https://www.simpleimageresizer.com/_uploads/photos/a5591a2e/4_2_optimized.png",
+        src: "https://i.ibb.co/6NHm8v8/4-1.png",
         alt: "Salon Interior"
       }
     },
     {
       mobile: {
-        src: "https://www.simpleimageresizer.com/_uploads/photos/a5591a2e/1_3_optimized.png",
+        src: "https://images.pexels.com/photos/8467976/pexels-photo-8467976.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        alt: "Salon Interior"
+      },
+      desktop: {
+        src: "https://images.pexels.com/photos/8467976/pexels-photo-8467976.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        alt: "Salon Interior"
+      }
+    },
+    {
+      mobile: {
+        src: "https://images.pexels.com/photos/1845160/pexels-photo-1845160.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        alt: "Salon Interior"
+      },
+      desktop: {
+        src: "https://images.pexels.com/photos/1845160/pexels-photo-1845160.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+        alt: "Salon Interior"
+      }
+    },
+    {
+      mobile: {
+        src: "https://i.ibb.co/30LXFp3/2.png",
         alt: "men hair"
       },
       desktop: {
-        src: "https://www.simpleimageresizer.com/_uploads/photos/a5591a2e/1_3_optimized.png",
+        src: "https://i.ibb.co/30LXFp3/2.png",
         alt: "men hair"
       }
     },
     {
       mobile: {
-        src: "https://www.simpleimageresizer.com/_uploads/photos/a5591a2e/3_5_optimized.png",
+        src: "https://i.ibb.co/7X165XH/3-2.png",
         alt: "men hair"
       },
       desktop: {
-        src: "https://www.simpleimageresizer.com/_uploads/photos/a5591a2e/3_4_optimized.png",
+        src: "https://i.ibb.co/7X165XH/3-2.png",
         alt: "men hair"
       }
     }
